f119352ab35b0a48e5ae567f09750d346e7806e2
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-05-29 15:37:17 +0900

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <algorithm>
+using namespace std;
 
-int main(int cargs, char **vargs) {
-    if (cargs < 1) perror("./main hogehuga");
+typedef pair<string, string> P;
 
+int main() {
+    map<string, string> dic;
+
+    // Make dictionary
+    string line;
+    ifstream dict ("/usr/share/dict/american-english");
+    if (dict.is_open())
+    {
+        while ( getline (dict,line) )
+        {
+            if(line.length() > 16)
+                continue;
+            if(line.find("'") != string::npos)
+                continue;
+            string val = line;
+            sort(line.begin(), line.end());
+            dic.insert(P(line, val));
+        }
+        dict.close();
+    }
+
+    // Compare user input and dictionary
+    for(;;) {
+        cout << ">> ";
+        string input;
+        cin >> input;
+
+        auto res = dic.find(input);
+        if ( res != dic.end() )
+            cout << res->second << endl;
+        else
+            cout << "not found..." << endl;
+    }
 
     return 0;
 }
