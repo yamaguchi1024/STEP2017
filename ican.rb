05f300e390c14890ad184c3b6ca9622c19d61954
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-05-30 18:07:54 +0900

    ruby

---
+++ ican.rb
@@ -0,0 +1,58 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+
+main = IO.popen('./main','w+')
+URL = "https://icanhazwordz.appspot.com/"
+uri = URI.parse(URL)
+
+# first gets
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(uri.request_uri)
+
+response = http.request(request)
+html = response.body
+
+moves = []
+seed = html.scan(/name=Seed value="(.*?)"/).flatten.join("")
+started = html.scan(/name=Started value="(.*?)"/).flatten.join("")
+p seed
+p started
+count = 0
+
+10.times do
+    chars = html.scan(/">(.)<\/div><\/td>/).flatten.join("")
+
+    main.puts chars
+    ans,num = main.gets.chomp.split(" ")
+    count += num.to_i
+    p moves
+
+    # post
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req.set_form_data({'Seed' => seed, 'Started' => started, 'move' => ans, 'Moves' => moves}, ';')
+
+    moves.push(ans)
+
+    res = Net::HTTP.new(uri.host, uri.port).tap{|h| h.use_ssl=true}.start do |http|
+        response = http.request(req)
+        html = response.body
+    end
+end
+
+p count
+if count >= 130 then
+
+    HIURL = "https://icanhazwordz.appspot.com/highscores"
+    hiuri = URI.parse(HIURL)
+
+    req = Net::HTTP::Post.new(hiuri.path)
+    req.set_form_data({'Seed' => seed, 'Started' => started, 'NickName' => 'Yuka', 'URL' => 'https://github.com/yamaguchi1024', 'Moves' => moves}, ';')
+
+    res = Net::HTTP.new(uri.host, uri.port).tap{|h| h.use_ssl=true}.start do |http|
+        response = http.request(req)
+        html = response.body
+    end
+end
