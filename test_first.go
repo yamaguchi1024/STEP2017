fb616f9989ad29dd43cdc195f59b145a0df36f0b
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-06-01 11:58:43 +0900

    hoge

---
+++ test_first.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var s = "JNZETDOSQLHBLEGU"
+	for i := 0; i < 1000000; i++ {
+		var flag = true
+		rand.Seed(int64(i))
+		for j := 0; j < 16; j++ {
+			if byte('A'+rand.Intn(26)) != s[j]  {
+				flag = false
+			}
+		}
+		if flag {
+			fmt.Printf("%d",i)
+			fmt.Println()
+		}
+	}
+}
