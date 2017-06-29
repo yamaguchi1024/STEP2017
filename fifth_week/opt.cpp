5be1db0177b535e24de68cceb69f76686e2f6d2a
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-06-29 09:12:17 +0900

    bug

--- fifth_week/opt.cpp
+++ fifth_week/opt.cpp
@@ -16,7 +16,7 @@ vector<int> route;
 int V = - 1;
 int used[MAX_V];
 
-double distance(P s, P e) {
+double dist(P s, P e) {
   return sqrt(pow(s.first - e.first, 2.0) + pow(s.second - e.second, 2.0));
 }
 
@@ -26,7 +26,7 @@ int find_min(int cur) {
   for (int i = 0; i < V; i++) {
     if (used[i]) continue;
 
-    if ( (tmp = distance(points[i], points[cur])) < min) {
+    if ( (tmp = dist(points[i], points[cur])) < min) {
       min = tmp;
       res = i;
     }
@@ -47,6 +47,52 @@ void greedy() {
   }
 }
 
+int next(int v) {
+  if (route[V - 1] == v)
+    return 0;
+  for (int i = 0; i < V - 1; i++)
+    if (route[i] == v)
+      return route[i+1];
+}
+
+void change(int a, int b) {
+  int a_i, b_i;
+  for (int i = 0; i < V; i++) {
+    if (route[i] == a)
+      a_i = i;
+    if (route[i] == b)
+      b_i = i;
+  }
+
+  cout << a_i << " " << b_i << endl;
+  route[a_i] = b;
+  route[b_i] = a;
+}
+
+int think(int s1, int d1, int s2, int d2) {
+  if (dist(points[s1],points[d2]) + dist(points[s2],points[d1]) < dist(points[s1],points[d1]) + dist(points[s2],points[d2])) {
+    change(d1,d2);
+    return 1;
+  }
+  return 0;
+}
+
+void opt() {
+  for(int h = 0;h < 1;h++) {
+    int better = 1;
+    for (int i = 0; i < V; i++) {
+      for (int j = 0; j < V; j++) {
+        if (i == j) continue;
+
+        if (think(i, next(i), j, next(j)))
+          better = 0;
+      }
+    }
+
+    if (better) break;
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     cout << "./main <filename>" << endl;
@@ -64,6 +110,10 @@ int main(int argc, char **argv) {
   }
 
   greedy();
+  change(0, 1993);
+  change(1993, 0);
+
+//  opt();
 
   cout << "index" << endl;
   for (int i : route)
