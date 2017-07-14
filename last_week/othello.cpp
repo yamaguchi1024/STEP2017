f10d8e017eeb1dcfc1ef4fa9b96e72833038d283
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-07-14 10:38:03 +0900

    bug

--- last_week/othello.cpp
+++ last_week/othello.cpp
@@ -4,11 +4,13 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
+#include <cstring>
 #include <stdlib.h>
 // Me is always 1
 
 using namespace std;
 typedef pair<int,int> P;
+vector<pair<P,int> > vec;
 int Eval[8][8] =
 { {  99, -8, 8, 6, 6, 8, -8, 99 },
   {  -8, -24, -4, -3, -3, -4, -24, -8 }, 
@@ -19,9 +21,16 @@ int Eval[8][8] =
   {  -8, -24,  -4, -3, -3, -4, -24,  -8 },
   {  99, -8, 8,  6,  6,  8,  -8, 99 } };
 
+bool sort_greater(const pair<P,int> &left, const pair<P,int> &right){
+  return left.second > right.second;
+}
+
 static void connect_alarm(int signo)
 {
-  printf("hoge\n");  
+  sort(vec.begin(),vec.end(),sort_greater);
+  P p = vec[0].first;
+  printf("%c%d\n",p.second + 'A', p.first + 1);
+
   exit(0);
 }
 
@@ -77,17 +86,79 @@ int Cstone(char Board[8][8]) {
   return cnt;
 }
 
-bool sort_greater(const pair<P,int> &left, const pair<P,int> &right){
-    return left.second > right.second;
+int calmax(const vector<pair<P,int> > &vec) {
+  P max_p;
+  int max_n = -500000;
+  for (int i = 0; i < vec.size(); i++) {
+    P p = vec[i].first;
+    int t = Eval[p.first][p.second] + vec[i].second;
+    if (t > max_n) {
+      max_n = t;
+      max_p = p;
+    }
+  }
+  return max_n;
+}
+
+int minmax(char Board[8][8], int player, int depth) {
+  if (depth == 0)
+    return Calc_eval(Board);
+
+  if (player == 1) {
+    vector<pair<P,int> > newvec = Placable(Board);
+
+    int max_b = -5000;
+    P max_p;
+    for (int i = 0; i < newvec.size(); i++) {
+      char newBoard[8][8];
+      memcpy(newBoard, Board, 64);
+      P p = newvec[i].first;
+      newBoard[p.first][p.second] = 1;
+      int t = minmax(newBoard, 0, depth - 1);
+      if (max_b < t) {
+        max_b = t;
+        max_p = p;
+      }
+    }
+    return max_b;
+  }
+
+  char revBoard[8][8];
+  memcpy(revBoard, Board, 64);
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if(revBoard[i][j] == '1')
+        revBoard[i][j] = '0';
+      else if(revBoard[i][j] == '0')
+        revBoard[i][j] = '1';
+    }
+  }
+
+  vector<pair<P,int> > newvec = Placable(revBoard);
+  int max_b = -5000;
+  P max_p;
+  for (int i = 0; i < newvec.size(); i++) {
+    char newBoard[8][8];
+    memcpy(newBoard, Board, 64);
+    P p = newvec[i].first;
+    newBoard[p.first][p.second] = 0;
+    int t = minmax(newBoard, 1, depth - 1);
+    if (max_b < -t) {
+      max_b = -t;
+      max_p = p;
+    }
+  }
+  return max_b;
 }
 
 int main() {
   signal(SIGALRM, connect_alarm);
-  alarm(12);
+  alarm(1000);
 
   char Board[8][8];
   for(int i = 0; i < 8; i++) scanf("%s", Board[i]);
-  vector<pair<P,int> > vec = Placable(Board);
+  vec = Placable(Board);
+
   if (vec.empty()) {
     printf("PASS");
     return 0;
@@ -101,16 +172,25 @@ int main() {
     return 0;
   }
 
+  for (pair<P,int> pa : vec) {
+    P p = pa.first;
+    printf("%c%d\n",p.second + 'A', p.first + 1);
+  }
+
+  int max_b = -5000;
   P max_p;
-  int max_n = -500000;
   for (int i = 0; i < vec.size(); i++) {
+    char newBoard[8][8];
+    memcpy(newBoard, Board, 64);
     P p = vec[i].first;
-    int t = Eval[p.first][p.second] + vec[i].second;
-    if (t > max_n) {
-      max_n = t;
+    newBoard[p.first][p.second] = 1;
+    int t = minmax(newBoard, 0, 10);
+    if (max_b < t) {
+      max_b = t;
       max_p = p;
     }
   }
   printf("%c%d\n",max_p.second + 'A', max_p.first + 1);
+
   return 0;
 }
